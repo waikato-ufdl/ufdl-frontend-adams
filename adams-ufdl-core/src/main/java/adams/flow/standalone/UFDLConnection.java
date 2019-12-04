36fb591e929d27e624bc4c4563b1bd404bb48bd7
Peter Reutemann <fracpete@gmail.com>
2019-12-04 17:20:33 +1300

    updated javadoc

--- adams-ufdl-core/src/main/java/adams/flow/standalone/UFDLConnection.java
+++ adams-ufdl-core/src/main/java/adams/flow/standalone/UFDLConnection.java
@@ -40,12 +40,83 @@ import java.util.List;
 
 /**
  <!-- globalinfo-start -->
+ * Defines the connection to the UFDL backend API.
+ * <br><br>
  <!-- globalinfo-end -->
  *
  <!-- flow-summary-start -->
  <!-- flow-summary-end -->
  *
  <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: UFDLConnection
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-host &lt;adams.core.base.BaseURL&gt; (property: host)
+ * &nbsp;&nbsp;&nbsp;The UFDL backend API host to use.
+ * &nbsp;&nbsp;&nbsp;default: http:&#47;&#47;localhost:8000
+ * </pre>
+ *
+ * <pre>-user &lt;java.lang.String&gt; (property: user)
+ * &nbsp;&nbsp;&nbsp;The SMTP user to use.
+ * </pre>
+ *
+ * <pre>-password &lt;adams.core.base.BasePassword&gt; (property: password)
+ * &nbsp;&nbsp;&nbsp;The password of the SMTP user.
+ * </pre>
+ *
+ * <pre>-prompt-for-password &lt;boolean&gt; (property: promptForPassword)
+ * &nbsp;&nbsp;&nbsp;If enabled and authentication is required, the user gets prompted for enter
+ * &nbsp;&nbsp;&nbsp;a password if none has been provided in the setup.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-if-canceled &lt;boolean&gt; (property: stopFlowIfCanceled)
+ * &nbsp;&nbsp;&nbsp;If enabled, the flow gets stopped in case the user cancels the dialog.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-custom-stop-message &lt;java.lang.String&gt; (property: customStopMessage)
+ * &nbsp;&nbsp;&nbsp;The custom stop message to use in case a user cancelation stops the flow
+ * &nbsp;&nbsp;&nbsp;(default is the full name of the actor)
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-stop-mode &lt;GLOBAL|STOP_RESTRICTOR&gt; (property: stopMode)
+ * &nbsp;&nbsp;&nbsp;The stop mode to use.
+ * &nbsp;&nbsp;&nbsp;default: GLOBAL
+ * </pre>
+ *
  <!-- options-end -->
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
