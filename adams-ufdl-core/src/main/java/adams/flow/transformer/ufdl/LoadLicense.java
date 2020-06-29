6d92602bf6c948371d7bcc047ee373fa1aef1d6b
Peter Reutemann <fracpete@gmail.com>
2020-06-29 20:25:03 +1200

    added action for loading a license

---
+++ adams-ufdl-core/src/main/java/adams/flow/transformer/ufdl/LoadLicense.java
@@ -0,0 +1,67 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * LoadLicense.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer.ufdl;
+
+import adams.core.MessageCollection;
+import com.github.waikatoufdl.ufdl4j.action.Licenses.License;
+
+/**
+ * Loads the license either via PK or license name.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class LoadLicense
+  extends AbstractLicenseTransformerAction {
+
+  private static final long serialVersionUID = 2890424326502728143L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Loads the license either via PK or license name.";
+  }
+
+  /**
+   * Returns the classes that the transformer generates.
+   *
+   * @return		the classes
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{License.class};
+  }
+
+  /**
+   * Transforms the license.
+   *
+   * @param input	the license
+   * @param errors 	for collecting errors
+   * @return 		the transformed data
+   */
+  @Override
+  protected Object doTransform(License input, MessageCollection errors) {
+    return input;
+  }
+}
