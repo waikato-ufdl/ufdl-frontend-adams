e85518210b1ffd079e997216373d75b855695248
Peter Reutemann <fracpete@gmail.com>
2020-06-30 16:17:34 +1200

    added actions for loading datasets

---
+++ adams-ufdl-image/src/main/java/adams/flow/transformer/ufdl/LoadObjectDetectionDataset.java
@@ -0,0 +1,75 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * LoadObjectDetectionDataset.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer.ufdl;
+
+import adams.core.MessageCollection;
+import com.github.waikatoufdl.ufdl4j.action.Datasets.Dataset;
+import com.github.waikatoufdl.ufdl4j.action.ObjectDetectionDatasets;
+import com.github.waikatoufdl.ufdl4j.action.ObjectDetectionDatasets.ObjectDetectionDataset;
+
+/**
+ * Loads the object detection dataset either via PK or dataset name.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class LoadObjectDetectionDataset
+  extends AbstractDatasetTransformerAction {
+
+  private static final long serialVersionUID = 2890424326502728143L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Loads the object detection dataset either via PK or dataset name.";
+  }
+
+  /**
+   * Returns the classes that the transformer generates.
+   *
+   * @return		the classes
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{ObjectDetectionDataset.class};
+  }
+
+  /**
+   * Transforms the dataset.
+   *
+   * @param dataset	the input data
+   * @param errors 	for collecting errors
+   * @return 		the transformed data
+   */
+  @Override
+  protected Object doTransform(Dataset dataset, MessageCollection errors) {
+    try {
+      return m_Client.action(ObjectDetectionDatasets.class).load(dataset.getPK());
+    }
+    catch (Exception e) {
+      errors.add("Failed to load object detection dataset: " + dataset.getPK(), e);
+      return null;
+    }
+  }
+}
