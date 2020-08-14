9f0212f5763eb15cf0caf41d492a2076894807ca
Peter Reutemann <fracpete@gmail.com>
2020-08-14 14:40:35 +1200

    added plugins for EnterManyValues for new backend endpoints

---
+++ adams-ufdl-core/src/main/java/adams/flow/source/valuedefinition/UFDLJobList.java
@@ -0,0 +1,87 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * UFDLJobList.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.source.valuedefinition;
+
+import adams.core.ClassCrossReference;
+import adams.flow.transformer.UFDLExtractAndTransfer;
+import com.github.fracpete.javautils.struct.Struct2;
+import com.github.waikatoufdl.ufdl4j.action.Jobs.Job;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+/**
+ * For selecting a UFDL job.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class UFDLJobList
+  extends AbstractUFDLSoftDeleteListValueDefinition
+  implements ClassCrossReference {
+
+  private static final long serialVersionUID = 4093023607556720026L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "For selecting a UFDL job.";
+  }
+
+  /**
+   * Returns the cross-referenced classes.
+   *
+   * @return		the classes
+   */
+  @Override
+  public Class[] getClassCrossReferences() {
+    return new Class[]{UFDLExtractAndTransfer.class};
+  }
+
+  /**
+   * Returns the list of items to display.
+   *
+   * @return		the items for the list (PK and description)
+   */
+  @Override
+  protected List<Struct2<Integer, String>> listItems() {
+    List<Struct2<Integer, String>>	result;
+
+    result = new ArrayList<>();
+
+    try {
+      for (Job job: m_Connection.getClient().jobs().list()) {
+        if (!m_State.accept(job))
+          continue;
+        result.add(new Struct2<>(job.getPK(), job.getNode()));
+      }
+    }
+    catch (Exception e) {
+      getLogger().log(Level.SEVERE, "Failed to retrieve list of jobs!", e);
+    }
+
+    return result;
+  }
+}
