8bf792cb184381c788a955e14e33f99692c42ccb
Peter Reutemann <fracpete@gmail.com>
2020-08-19 16:50:09 +1200

    for turning a job output into a spreadsheet

---
+++ adams-ufdl-core/src/main/java/adams/data/conversion/UFDLJobOutputToSpreadSheet.java
@@ -0,0 +1,97 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * UFDLJobOutputToSpreadSheet.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ */
+
+package adams.data.conversion;
+
+import adams.data.spreadsheet.DefaultSpreadSheet;
+import adams.data.spreadsheet.Row;
+import adams.data.spreadsheet.SpreadSheet;
+import com.github.waikatoufdl.ufdl4j.action.Jobs.JobOutput;
+
+/**
+ * Converts a JobOutput object into a Spreadsheet.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class UFDLJobOutputToSpreadSheet
+  extends AbstractUFDLObjectToSpreadSheetConversion {
+
+  private static final long serialVersionUID = 2239463804853893127L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Converts a JobOutput object into a Spreadsheet.";
+  }
+
+  /**
+   * Returns the class that is accepted as input.
+   *
+   * @return		the class
+   */
+  @Override
+  public Class accepts() {
+    return JobOutput.class;
+  }
+
+  /**
+   * Generates the template.
+   *
+   * @return		the template
+   */
+  @Override
+  public SpreadSheet getTemplate() {
+    SpreadSheet result;
+    Row 	row;
+
+    result = new DefaultSpreadSheet();
+
+    row = result.getHeaderRow();
+    row.addCell("na").setContentAsString("name");
+    row.addCell("ty").setContentAsString("type");
+
+    return result;
+  }
+
+  /**
+   * Performs the actual conversion.
+   *
+   * @return		the converted data
+   * @throws Exception	if something goes wrong with the conversion
+   */
+  @Override
+  protected Object doConvert() throws Exception {
+    SpreadSheet		result;
+    Row			row;
+    JobOutput 		output;
+
+    output = (JobOutput) m_Input;
+    result = getTemplate();
+    row    = result.addRow();
+    row.addCell("na").setContent(output.getName());
+    row.addCell("ty").setContent(output.getType());
+
+    return result;
+  }
+}
