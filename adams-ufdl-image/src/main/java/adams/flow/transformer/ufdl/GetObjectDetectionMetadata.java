a44f9346f033409328d59e017e5c1734c33482d3
Peter Reutemann <fracpete@gmail.com>
2020-09-21 12:24:33 +1200

    added ability to retrieve meta-data for dataset rather than only per individual file

---
+++ adams-ufdl-image/src/main/java/adams/flow/transformer/ufdl/GetObjectDetectionMetadata.java
@@ -0,0 +1,80 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * GetObjectDetectionMetadataForFile.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer.ufdl;
+
+import adams.core.MessageCollection;
+import com.github.waikatoufdl.ufdl4j.action.Datasets.Dataset;
+
+import java.util.Map;
+
+/**
+ * Obtains the metadata for the dataset passing through and forwards it.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class GetObjectDetectionMetadata
+  extends AbstractObjectDetectionDatasetTransformerAction {
+
+  private static final long serialVersionUID = -1421130988687306299L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Obtains the metadata for the dataset passing through and forwards it.\n"
+      + "JSON object has filename - metadata string relationship.";
+  }
+
+  /**
+   * Returns the classes that the transformer generates.
+   *
+   * @return		the classes
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{Map.class};
+  }
+
+  /**
+   * Transforms the input data.
+   *
+   * @param dataset	the input data
+   * @param errors 	for collecting errors
+   * @return 		the transformed data
+   */
+  @Override
+  protected Object doTransform(Dataset dataset, MessageCollection errors) {
+    Map	result;
+
+    result = null;
+    try {
+      result = getDatasetsAction().getMetadata(dataset);
+    }
+    catch (Exception e) {
+      errors.add("Failed to retrieve metadata for dataset: " + dataset, e);
+    }
+
+    return result;
+  }
+}
