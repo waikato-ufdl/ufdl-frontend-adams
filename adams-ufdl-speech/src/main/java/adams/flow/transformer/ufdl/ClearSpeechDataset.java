7c7a95e22c5501975683169cdd3decdddb4fdcb4
Peter Reutemann <fracpete@gmail.com>
2020-10-07 12:22:27 +1300

    added "clear" operations for datasets

---
+++ adams-ufdl-speech/src/main/java/adams/flow/transformer/ufdl/ClearSpeechDataset.java
@@ -0,0 +1,82 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ClearSpeechDataset.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer.ufdl;
+
+import adams.core.MessageCollection;
+import com.github.waikatoufdl.ufdl4j.action.Datasets.Dataset;
+import com.github.waikatoufdl.ufdl4j.action.SpeechDatasets.SpeechDataset;
+
+/**
+ * Clears the speech dataset either via PK or datasetname.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class ClearSpeechDataset
+  extends AbstractSpeechDatasetTransformerAction {
+
+  private static final long serialVersionUID = 2890424326502728143L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Clears the speech dataset either via PK or dataset name.";
+  }
+
+  /**
+   * Returns the classes that the transformer generates.
+   *
+   * @return		the classes
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{SpeechDataset.class};
+  }
+
+  /**
+   * Transforms the dataset.
+   *
+   * @param dataset	the dataset
+   * @param errors 	for collecting errors
+   * @return 		the transformed data
+   */
+  @Override
+  protected Object doTransform(Dataset dataset, MessageCollection errors) {
+    Dataset result;
+
+    result = null;
+
+    if (isLoggingEnabled())
+      getLogger().info("Clearing dataset: " + dataset);
+
+    try {
+      result = getDatasetsAction().clear(dataset);
+    }
+    catch (Exception e) {
+      errors.add("Failed to clear dataset: " + dataset, e);
+    }
+
+    return result;
+  }
+}
