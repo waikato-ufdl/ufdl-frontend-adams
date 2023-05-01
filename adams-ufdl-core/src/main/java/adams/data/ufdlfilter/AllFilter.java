cfcfc8d23750358c6ba6a1b6a6b54b2fb39fe31e
Peter Reutemann <fracpete@gmail.com>
2023-05-01 16:19:28 +1200

    now lists inactive entries by default

--- adams-ufdl-core/src/main/java/adams/data/ufdlfilter/AllFilter.java
+++ adams-ufdl-core/src/main/java/adams/data/ufdlfilter/AllFilter.java
@@ -15,13 +15,15 @@
 
 /*
  * AllFilter.java
- * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ * Copyright (C) 2020-2023 University of Waikato, Hamilton, NZ
  */
 
 package adams.data.ufdlfilter;
 
 import adams.core.MessageCollection;
+import com.github.waikatoufdl.ufdl4j.filter.AbstractExpression;
 import com.github.waikatoufdl.ufdl4j.filter.Filter;
+import com.github.waikatoufdl.ufdl4j.filter.OrderBy;
 
 /**
  * Dummy filter, performs no filtering.
@@ -32,6 +34,9 @@ public class AllFilter
   extends AbstractUFDLFilter {
 
   private static final long serialVersionUID = 2812847375649367286L;
+
+  /** whether to include inactive elements. */
+  protected boolean m_IncludeInactive;
 
   /**
    * Returns a string describing the object.
@@ -44,13 +49,55 @@ public class AllFilter
   }
 
   /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "include-inactive", "includeInactive",
+      true);
+  }
+
+  /**
+   * Sets whether to include inactive items in the result.
+   *
+   * @param value	true if to include
+   */
+  public void setIncludeInactive(boolean value) {
+    m_IncludeInactive = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to include inactive items in the result.
+   *
+   * @return		true if to include
+   */
+  public boolean getIncludeInactive() {
+    return m_IncludeInactive;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String includeInactiveTipText() {
+    return "If enabled, inactive results will be included in the results.";
+  }
+
+  /**
    * Generates the filter.
    *
    * @param errors	for collecting errors
-   * @return		always null
+   * @return		the filter
    */
   @Override
   protected Filter doGenerate(MessageCollection errors) {
-    return null;
+    return new com.github.waikatoufdl.ufdl4j.filter.GenericFilter(
+      new AbstractExpression[0], new OrderBy[0], m_IncludeInactive);
   }
 }
